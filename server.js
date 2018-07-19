ef604eeb9c8fd37832f048450484e1bc404c7554
F3n1xLaurent <f3n1xfsociety@mail.ru>
2018-07-19 23:40:04 +0300

    Версия с проверкой авторизации и с началом дизайна панели

--- server.js
+++ server.js
@@ -5,13 +5,29 @@ var app = express();
 var cookieParser = require('cookie-parser');
 app.use(cookieParser());
 
+var session = require('express-session');
+var MySQLStore = require('express-mysql-session')(session);
+
+var options = {
+	host: 'localhost',
+	port: 3306,
+	user: 'root',
+	password: '',
+	database: 'panel'
+};
+
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-	host : 'localhost',
-	user : 'root',
-	password : '',
-	database : 'panel'
-});
+var connection = mysql.createConnection(options);
+
+var sessionStore = new MySQLStore(options);
+
+app.use(session({
+	key: 'sid',
+	secret: 'F3n1xLaurent',
+	store: sessionStore,
+	resave: false,
+	saveUninitialized: false
+}));
 
 connection.connect();
 
@@ -23,19 +39,17 @@ const util = require('util');
 const setTimeoutPromise = util.promisify(setTimeout);
 
 app.post("/clearcookie", function(req, res){
-	res.cookie('login', '', { maxAge: 900000, httpOnly: true });
+	req.session.destroy();
 	res.redirect('/');
 });
 
 app.get("/", function(req, res){
 	//console.log(cookie);
-	if(req.cookies['login'] !== ""){
-		console.log(req.cookies['login']);
-		res.render('panel.ejs');
-	//} else if (!(cookie == "")){
-	//	res.render('panel.ejs');
+	if(req.session.auth){
+		//console.log(req.cookies['login']);
+		res.render('panel.ejs',{user: req.session.auth});
 	} else {
-		console.log(req.cookies['login']);
+		//console.log(req.cookies['login']);
 		res.sendFile(`${__dirname}/public/startpage.html`);
 	}
 });
@@ -47,13 +61,19 @@ app.post("/signup", function(req, res){
 	var password = req.body.password;
 	var email = req.body.email;
 
-	connection.query("INSERT INTO `users` (`id`, `login`, `pass`, `email`) VALUES (NULL, '"+ login +"', '"+ password +"', '"+ email +"')", function(err,rows,fields){
-		if(err){
-			res.send('Ошибка в регистрации, попробуйте позже!');
-		} else {
-			res.send('Вы успешно зарегистрированны!');
-		}
-	});
+			connection.query("SELECT * FROM users WHERE login = '" + login + "' OR email = '" + email + "'", function(err,rows,fields){
+				if(rows.length > 0){
+					res.send('Пользователь с таким именем или почтой уже существует!');
+				} else {
+					connection.query("INSERT INTO `users` (`id`, `login`, `pass`, `email`) VALUES (NULL, '"+ login +"', '"+ password +"', '"+ email +"')", function(err,rows,fields){
+						if(err){
+							res.send('Ошибка в регистрации, попробуйте позже!');
+						} else {
+						res.send('Вы успешно зарегистрированны!');
+						}
+					})
+				}
+			});
 });
 
 app.post("/login", function(req, res){
@@ -64,9 +84,10 @@ app.post("/login", function(req, res){
 		if(!rows.length == 0){
 			if(password == rows[0].pass){
 				//res.clearCookie('login');
-				res.cookie('login', login, { maxAge: 900000, httpOnly: true });
+				req.session.auth = {'login':login,'pass':password};
+				res.locals.login = login; 
 				res.send('Авторизованы!');
-				console.log(req.cookies['login']);
+				//console.log(req.cookies['login']);
 			} else {
 				res.send('Ошибка в логине или пароле');
 			}
